3c966d0d284602283cd0291ed386b3b2dec62bb1
nikithakethireddy1996 <54380887+nikithakethireddy1996@users.noreply.github.com>
2019-11-08 14:23:38 -0600

    commit

--- controllers/Section.js
+++ controllers/Section.js
@@ -32,7 +32,86 @@ api.get('/findone/:id', (req, res) => {
 
 // RESPOND WITH VIEWS  --------------------------------------------
 
-// later
+// GET to this controller base URI (the default)
+api.get('/', (req, res) => {
+  res.render('Section/index.ejs', {
+    Sections: req.app.locals.Sections.query
+  })
+})
+
+// GET create
+api.get('/create', (req, res) => {
+  res.render('Section/create', {
+    Sections: req.app.locals.Sections.query,
+    Section: new Model()
+  })
+})
+
+// GET /delete/:id
+api.get('/delete/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  const data = req.app.locals.Sections.query
+  const item = find(data, { _id: id })
+  if (!item) { return res.end(notfoundstring + id) }
+  res.render('Section/delete', {
+    Section: item
+  })
+})
+
+// GET /details/:id
+api.get('/details/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  const data = req.app.locals.Sections.query
+  const item = find(data, { _id: id })
+  if (!item) { return res.end(notfoundstring + id) }
+  res.render('Section/details', {
+    Section: item
+  })
+})
+
+// GET one
+api.get('/edit/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  const data = req.app.locals.Sections.query
+  const item = find(data, { _id: id })
+  if (!item) { return res.end(notfoundstring + id) }
+  res.render('Section/edit', {
+    Section: item
+  })
+})
+
+// HANDLE EXECUTE DATA MODIFICATION REQUESTS --------------------------------------------
+
+// POST new
+api.post('/save', (req, res) => {
+  console.info(`Handling POST ${req}`)
+  console.debug(JSON.stringify(req.body))
+  const item = new Model()
+  console.info(`NEW ID ${req.body._id}`)
+  item._id = parseInt(req.body._id)
+  item.SectionNumber  = req.body.SectionNumber 
+  item.Days = req.body.Days
+  item.StartTime = req.body.StartTime
+  item.RoomNumber = req.body.RoomNumber
+  item.CourseID = req.body.CourseID
+  res.send(`THIS FUNCTION WILL SAVE A NEW Section ${JSON.stringify(item)}`)
+})
+
+// POST update with id
+api.post('/save/:id', (req, res) => {
+  console.info(`Handling SAVE request ${req}`)
+  const id = parseInt(req.params.id)
+  console.info(`Handling SAVING ID=${id}`)
+  res.send(`THIS FUNCTION WILL SAVE CHANGES TO AN EXISTING Section with id=${id}`)
+})
+
+// DELETE id (uses HTML5 form method POST)
+api.post('/delete/:id', (req, res) => {
+  console.info(`Handling DELETE request ${req}`)
+  const id = parseInt(req.params.id)
+  console.info(`Handling REMOVING ID=${id}`)
+  res.send(`THIS FUNCTION WILL DELETE FOREVER THE EXISTING Section with id=${id}`)
+})
 
 // RESPOND WITH DATA MODIFICATIONS  -------------------------------
 
